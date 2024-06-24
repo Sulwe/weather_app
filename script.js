95ae7dae307084fa52836e85bf794aab727500a8
Sulwe <cypriansulwe.cs@gmail.com>
2024-06-24 14:56:40 +0300

    add code to get location form user input

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
 const apiKey = "2cd3ccded2e644579dc113142240406";
-const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=nairobi`;
 
-
-async function queryWeather() {
+async function queryWeather(city) {
+    const lcity = searchBox.value;
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lcity}`;
     const response = await fetch(apiUrl);
     let data = await response.json();
 
     console.log(data);
+
+    document.querySelector(".city").innerHTML = data.location.name;
+    document.querySelector(".temp").innerHTML = `${data.current.temp_c}°C`;
+    document.querySelector(".humidity").innerHTML = `${data.current.humidity}%`;
+    document.querySelector(".wind").innerHTML = `${data.current.wind_kph} kph`;
+
 }
 
-queryWeather()
+searchBtn.addEventListener('click', () => {
+
+    queryWeather();
+})
