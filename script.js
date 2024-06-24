139128cf7b44e73e778c3e256ce8cba3c3acdeca
Sulwe <cypriansulwe.cs@gmail.com>
2024-06-24 16:31:49 +0300

    add code to change weather images and handle error in names

--- script.js
+++ script.js
@@ -7,14 +7,50 @@ async function queryWeather(city) {
     const lcity = searchBox.value;
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lcity}`;
     const response = await fetch(apiUrl);
-    let data = await response.json();
 
-    console.log(data);
+    if(response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
 
-    document.querySelector(".city").innerHTML = data.location.name;
-    document.querySelector(".temp").innerHTML = `${data.current.temp_c}°C`;
-    document.querySelector(".humidity").innerHTML = `${data.current.humidity}%`;
-    document.querySelector(".wind").innerHTML = `${data.current.wind_kph} kph`;
+    }
+    else {
+        let data = await response.json();
+        const weatherIcon = document.querySelector(".weather-icon");
+    
+        console.log(data);
+    
+        document.querySelector(".city").innerHTML = data.location.name;
+        document.querySelector(".temp").innerHTML = `${data.current.temp_c}°C`;
+        document.querySelector(".humidity").innerHTML = `${data.current.humidity}%`;
+        document.querySelector(".wind").innerHTML = `${data.current.wind_kph} kph`;
+    
+        if(data.current.condition.text == "Partly cloudy" || data.current.condition.text ==  "cloudy" || data.current.condition.text ==  "Overcast"){
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if(data.current.condition.text == "Clear" || data.current.condition.text == "Sunny"){
+            weatherIcon.src = "images/clear.png";
+        }
+        else if(data.current.condition.text == "Patchy rain possible" || data.current.condition.text == "Light drizzle" || data.current.condition.text == "Patchy light drizzle"){
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if(data.current.condition.text == "Light Rain" || data.current.condition.text == "Moderate rain" || data.current.condition.text == "Heavy rain"){
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.current.condition.text == "Mist"){
+            weatherIcon.src = "images/mist.png";
+        }
+        else if(data.current.condition.text == "Patchy light snow" || data.current.condition.text == "Moderate snow" || data.current.condition.text == "Heavy Snow"){
+            weatherIcon.src = "images/snow.png";
+        }
+        else {
+            weatherIcon.src = "images/clouds.png";
+        }
+    
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+
+    }
+
 
 }
 
